ad15f19ccf4bedde99d21d9da627144de5d3a080
Sebastian Garcia <121516129+SebCodes27@users.noreply.github.com>
2023-08-22 16:51:58 -0700

    Changed button widget links

---
+++ index.js
@@ -0,0 +1,22 @@
+function myFunction() {
+    let x = document.getElementById("mySecondNav");
+    if (x.className === "second-nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "second-nav";
+    }
+}
+function calWidget() {
+    let popup = document.getElementById('myPopup');
+    popup.classList.toggle('show');
+    let calendar = document.getElementById('msgsndr-calendar');
+    if (calendar.src !== 'https://api.leadconnectorhq.com/widget/booking/51s99XpkQ63NwU4eLIwQ') {
+        calendar.src = 'https://api.leadconnectorhq.com/widget/booking/51s99XpkQ63NwU4eLIwQ'
+    }
+    let exitButton = document.getElementById('exit-button');
+    exitButton.classList.toggle('show');
+}
+function exitWidget() {
+    calWidget();
+
+}
