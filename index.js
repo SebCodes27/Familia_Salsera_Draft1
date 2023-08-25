fee3df1f1b1bd184da652e1ad92f3cd3209777e3
Sebastian Garcia <121516129+SebCodes27@users.noreply.github.com>
2023-08-25 13:32:41 -0700

    Calendar changes
    Fixed calendar link and added translucent background

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ function myFunction() {
 function calWidget() {
     let popup = document.getElementById('myPopup');
     popup.classList.toggle('show');
+    let calFade = document.getElementById('calFade');
+    calFade.classList.toggle('show');
     let calendar = document.getElementById('msgsndr-calendar');
     if (calendar.src !== 'https://api.leadconnectorhq.com/widget/booking/51s99XpkQ63NwU4eLIwQ') {
         calendar.src = 'https://api.leadconnectorhq.com/widget/booking/51s99XpkQ63NwU4eLIwQ'
